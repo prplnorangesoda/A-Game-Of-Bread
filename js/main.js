128e929e661df12ec75834c2866350e28838f6a2
Rafplayz <60671867+Rafplayz@users.noreply.github.com>
2021-03-22 17:32:28 -0400

    fk

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 var addAmount = new Decimal(1)
-var numToAddTo = new Decimal(0)
+var numToAddTo = new Decimal(1)
 document.getElementById("test").innerText = numToAddTo
 document.getElementById("addNumber").innerText = `Add ${addAmount} to the amount of bread`
 function addNum() {
-    let CurrentAmount = numToAddTo
-    let newAmount = CurrentAmount + addAmount
-    document.getElementById("test").innertext = newAmount
+    numToAddTo = numToAddTo + addAmount
+    console.log(numToAddTo)
 }
