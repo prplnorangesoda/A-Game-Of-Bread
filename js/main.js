72a5d79082a7412f6208a70fc61f444ba690a88d
Rafplayz <60671867+Rafplayz@users.noreply.github.com>
2021-04-07 09:41:26 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,32 +2,32 @@ document.addEventListener("DOMContentLoaded", loadShit, false);
 var addAmount = new Decimal(1)
 var numToAddTo = new Decimal(1)
 var Cost = new Decimal(10)
+var BreadGenerators = new Decimal(0)
+var GenCost = new Decimal(100)
 function Update() {
 }
 function loadShit() { // idfk why but this shit doesn't load half the time
     console.log("shit loaded")
-    document.getElementById("bodytypebeat").innerHTML = '<button id="addNumber" onclick="addNum()">loading...</button> <button id="buy" onclick="moreBread()">test</button><p id="test">0</p>'
+    document.getElementById("bodytypebeat").innerHTML = '<button id="addNumber" onclick="addNum()">loading...</button> <button id="buy" onclick="moreBread()">test</button><button id="genBuy" onclick="breadGenerator">100</button><p id="test">0</p>'
     document.getElementById("test").innerText = numToAddTo
     document.getElementById("addNumber").innerText = `Add ${addAmount} to the amount of bread`
     document.getElementById("buy").innerText = `Add +1 to bread adder. Cost: ${Cost}`
+    document.getElementById("genBuy").innerText = `Generate +1 bread per second. Cost: ${GenCost}`
 }
-function pageFullyLoaded() {
-    
-}
-var TestVar = 0
 var Interval = window.setInterval(Update, 50);
-
-
 function Update(){
-    TestVar++;
-    console.log(TestVar)
+    var BreadPerTick = getBreadPerTick()
+    numToAddTo = numToAddTo.plus(BreadPerTick)
+    document.getElementById("test").innerText = numToAddTo
 }
 function addNum() {
     numToAddTo = numToAddTo.plus(addAmount)
     document.getElementById("test").innerText = numToAddTo
 }
-function getBreadPerSecond() {
-    return addAmount.times(5) // with 5 cps
+function getBreadPerTick() {
+    var ReturnVar = new Decimal(0) // the variable we will return
+    ReturnVar = ReturnVar.plus(BreadGenerators*0.05)
+    return ReturnVar
 }
 function moreBread() {
     if(numToAddTo.gte(Cost)) {
@@ -37,6 +37,20 @@ function moreBread() {
         addAmount = addAmount.plus(1)
         document.getElementById("test").innerText = numToAddTo
         document.getElementById("buy").innerText = `Add +1 to bread adder. Cost: ${Cost}`
+        document.getElementById("addNumber").innerText = `Add ${addAmount} to the amount of bread`
+    }
+    else {
+        return null
+    }
+}
+function breadGenerator() {
+    if(numToAddTo.gte(GenCost)) {
+        numToAddTo = numToAddTo.minus(GenCost)
+        GenCost = GenCost * 1.3
+        GenCost = Decimal.round(GenCost)
+        breadGenerators = breadGenerators.plus(1)
+        document.getElementById("test").innerText = numToAddTo
+        document.getElementById("genBuy").innerText = `Generate +1 bread per second. Cost: ${GenCost}`
         document.getElementById("addNumber").innerText = `Add ${addAmount} to the amount of bread`
     }
     else {
