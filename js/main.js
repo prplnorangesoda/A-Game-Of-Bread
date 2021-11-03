a5125d423dc49e75199a3cdeed9829f4cab22967
Rafplayz <60671867+Rafplayz@users.noreply.github.com>
2021-11-03 13:14:14 -0400

    Basis for future changes

--- js/main.js
+++ js/main.js
@@ -6,20 +6,22 @@ var BreadGenerators = new Decimal(0)
 var GenCost = new Decimal(100)
 var breadUp1 = false
 var displayingbreadUp1 = false
-function Update() {
-}
+var breadAdderAmount = new Decimal(1)
+var breadGenPerSecAmount = new Decimal(1)
+function Update() {}
 function loadShit() { // idfk why but this shit doesn't load half the time
     document.getElementById("bodytypebeat").innerHTML = '<button id="addNumber" onclick="addNum()">loading...</button> <button id="buy" onclick="moreBread()">test</button> <button id="genBuy" onclick="breadGenerator()">100</button><p id="test">0</p><br><div id=upgrades></div>'
     document.getElementById("test").innerText = numToAddTo
     document.getElementById("addNumber").innerText = `Add ${addAmount} to the amount of bread`
-    document.getElementById("buy").innerText = `Add +1 to bread adder. Cost: ${Cost}`
-    document.getElementById("genBuy").innerText = `Generate +1 bread per second. Cost: ${GenCost}`
+    document.getElementById("buy").innerText = `Add +${breadAdderAmount} to bread adder. Cost: ${Cost}`
+    document.getElementById("genBuy").innerText = `Generate +${breadGenPerSecAmount} bread per second. Cost: ${GenCost}`
 }
 function breadUp1Func() {
     if(numToAddTo.gte(200)) {
         numToAddTo = numToAddTo.minus(200)
         document.getElementById("test").innerText = numToAddTo.mag.toFixed(2)
         breadUp1 = true
+        breadGenPerSecAmount = breadGenPerSecAmount.plus(1)
         document.getElementById("breadUp1Button").style = "display:none;"
     }
 }    
@@ -64,7 +66,7 @@ function breadGenerator() {
         GenCost = Decimal.round(GenCost)
         BreadGenerators = BreadGenerators.plus(1)
         document.getElementById("test").innerText = numToAddTo.mag.toFixed(2)
-        document.getElementById("genBuy").innerText = `Generate +1 bread per second. Cost: ${GenCost}`
+        document.getElementById("genBuy").innerText = `Generate +${breadGenPerSecAmount} bread per second. Cost: ${GenCost}`
         document.getElementById("addNumber").innerText = `Add ${addAmount} to the amount of bread`
     }
     else {
